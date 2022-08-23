1708c6b830f943865c56ac72e72ce01163e0bd36
Sergei Johansen <sergeitheprogrammer@gmail.com>
2022-08-23 16:24:36 +0200

    Removed everything, started from scratch, added general styles and dependencies

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
-import MenuContent from "./components/Content/MenuContent";
-import AboutUsPage from "./pages/AboutUsPage";
-import HomePage from "./pages/HomePage";
+
+
 
 
 function App() {
   return (
-    <MenuContent />
+    <div>
+      <h1>Oriental Thaimassasje Trondheim</h1>
+      <h2>H2 Title</h2>
+      <h3>H3 Title</h3>
+      <p>Paragraph wdiowejd iowejdio dioewo ijiewj diwje weijiiwej jidwii<br></br>Paragraph wdiowejd iowejdio dioewo ijiewj diwje weijiiwej jidwii</p>
+      <p>Paragraph wdiowejd iowejdio dioewo ijiewj diwje weijiiwej jidwii<br></br>Paragraph wdiowejd iowejdio dioewo ijiewj diwje weijiiwej jidwii</p>
+      <p>Paragraph wdiowejd iowejdio dioewo ijiewj diwje weijiiwej jidwii<br></br>Paragraph wdiowejd iowejdio dioewo ijiewj diwje weijiiwej jidwii</p>
+      <span>ewiofwioefnwoiefjiowe jioewfj woeifj w</span>
+      <a href="/">Click on me</a>
+      <button>Discover</button>
+    </div>
   );
 }
 
